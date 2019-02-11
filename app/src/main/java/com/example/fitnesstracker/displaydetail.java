4dd362165f8d56398ebd097a61d594a1409410f4
msharif1997 <abdisharif2015@gmail.com>
2019-02-10 21:30:50 -0600

    first commit

---
+++ app/src/main/java/com/example/fitnesstracker/displaydetail.java
@@ -0,0 +1,13 @@
+package com.example.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displaydetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaydetail);
+    }
+}
